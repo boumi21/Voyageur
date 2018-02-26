e22c1499cf38d15baf6d2f37c742d71026409f29
lucas <lucas.paulin@hotmail.fr>
2018-02-26 14:46:44 +0100

    Ajout des classes

--- src/Edge.java
+++ src/Edge.java
@@ -2,4 +2,8 @@
  * Created by lucas on 26/02/2018.
  */
 public class Edge {
+
+    public Node start;
+    public Node end;
+    public double valeur;
 }
--- src/Node.java
+++ src/Node.java
@@ -1,18 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by lucas on 26/02/2018.
  */
 public class Node {
 
-    public String ville;
-    public int numLinks;
-
-    public Edge[] voisins;
-
-    public Node(String ville){
-        this.ville = ville;
-    }
-
-    public void setVoisins(Node start, Node end, int cost){
-        
-    }
+    public String nom;
+    public List<Edge> connections;
 }
